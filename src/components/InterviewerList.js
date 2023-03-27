01a8dc557172a6e17a548123ee9f3ad65f94dca6
Ozzy Tasci <oguzcantasci@gmail.com>
2023-03-26 23:22:04 -0400

    Remove 'name' prop from InterviewListItem component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -11,7 +11,6 @@ export default function InterviewerList(props) {
           return (
             <InterviewerListItem
               key={interviewer.id}
-              name={interviewer.name}
               avatar={interviewer.avatar}
               selected={interviewer.id === props.interviewer}
               setInterviewer={(event)=>props.setInterviewer(interviewer.id)}
