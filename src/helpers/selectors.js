e9ab5dea3b03de87bb6b3b8bb9e3dbff6e655583
Ozzy Tasci <oguzcantasci@gmail.com>
2023-04-12 05:32:41 -0400

    Implement getInterviewersForDay selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,4 +9,23 @@ export function getAppointmentsForDay (state, day) {
   return appointments;
 }
 
+export function getInterviewersForDay (state, day) {
+  
+    const dayObject = state.days.find((dayObject) => dayObject.name === day);
+    if (!dayObject) {
+      return [];
+    }
+  
+    const interviewers = dayObject.interviewers.map((interviewer) => state.interviewers[interviewer]);
+    return interviewers;
+}
+
+export function getInterview (state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewer = state.interviewers[interview.interviewer];
+  return { ...interview, interviewer };
+}
+
 
