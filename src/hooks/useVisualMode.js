c25133d4fad052d3d2818cc9779d5ec0fbf2d0b3
Ozzy Tasci <oguzcantasci@gmail.com>
2023-04-12 05:31:29 -0400

    Implement useVisualMode custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import {React, useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      history.pop();
+    }
+    setHistory([...history, newMode]);
+    setMode(newMode);
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
