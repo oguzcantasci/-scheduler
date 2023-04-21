dc6d2db75203908985e8eac66b5ce5f4e8a91c57
Ozzy Tasci <oguzcantasci@gmail.com>
2023-04-21 12:52:04 -0400

    Add comments

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,10 +1,11 @@
 import { renderHook, act } from "@testing-library/react-hooks";
-
 import useVisualMode from "hooks/useVisualMode";
 
+//Modes for the useVisualMode hook
 const FIRST = "FIRST";
 const SECOND = "SECOND";
-const THIRD = "THIRD";
+const THIRD = 'THIRD';
+
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -48,7 +49,7 @@ test("useVisualMode should replace the current mode", () => {
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
 
-  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  //Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
   act(() => result.current.transition(THIRD, true));
   expect(result.current.mode).toBe(THIRD);
 
